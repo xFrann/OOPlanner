feeef69d2862f33d9c95ca4945639a0e5450427e
xFrann <polarfrann@gmail.com>
2020-08-21 14:26:01 +0300

    Initial commit

---
+++ src/main/java/xyz/polarfrann/ooplanner/Planner.java
@@ -0,0 +1,11 @@
+package xyz.polarfrann.ooplanner;
+/*
+    Planner Class for OOPlanner
+    Created on 21/08/2020 at 14:17
+    by frann
+
+    Main Project Class
+*/
+
+public class Planner {
+}
---
+++ src/test/java/xyz/polarfrann/ooplanner/tests/PrimaryTests.java
@@ -0,0 +1,13 @@
+package xyz.polarfrann.ooplanner.tests;
+/*
+    PrimaryTests for OOPlanner
+    Created on 21/08/2020 at 14:13
+    by frann
+
+    This is my first project using unit testing
+    I have no clue on how it's supposed to be used
+    but i will be learning while coding
+*/
+
+public class PrimaryTests {
+}
