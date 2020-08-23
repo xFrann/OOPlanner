c6079e0973bb960f8d8a97c1416c82ae69842d95
xFrann <polarfrann@gmail.com>
2020-08-23 12:10:28 +0300

    Setup a tree view (project explorer)

--- src/main/java/xyz/polarfrann/ooplanner/FXController.java
+++ src/main/java/xyz/polarfrann/ooplanner/FXController.java
@@ -5,8 +5,45 @@ package xyz.polarfrann.ooplanner;
     by frann
 */
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FXController {
 
+    @FXML
+    TreeView<String> explorer;
 
+    @FXML
+    public void initialize() {
+    fillTree();
+    }
+    /*
+        Dummy tree for testing purposes
+     */
+    public void fillTree() {
+        TreeItem<String> projectFolder = new TreeItem<>("Project Name");
+        projectFolder.setExpanded(true);
+        addToTree(projectFolder);
+        explorer.setRoot(projectFolder);
+
+    }
+    public void addToTree(TreeItem<String> item) {
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get("/home/frann/Code/"))) {
+            for (Path path : directoryStream) {
+                TreeItem<String> newItem = new TreeItem<>(path.toString().substring(path.toString().lastIndexOf("/")));
+                newItem.setExpanded(true);
+                item.getChildren().add(newItem);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
